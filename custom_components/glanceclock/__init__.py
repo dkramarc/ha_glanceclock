8ea113d3adc597f271fa3a1e709f8b838e8fa80b
Doron Kramarczyk <dkramarc@gmail.com>
2024-06-26 15:14:23 -0400

    Start of Project

---
+++ custom_components/glanceclock/__init__.py
@@ -0,0 +1,8 @@
+"""Glance Clock integration."""
+
+import asyncio
+from bleak import BleakClient
+
+async def async_setup(hass, config):
+    """Set up the Glance Clock component."""
+    return True
